46882933c5f56cb393ef2be7ae8e49f39c6375b8
Samuel Yu <samuelyuyy@gmail.com>
2021-04-12 00:05:46 +0800

    Move plot result code to function

--- hw2-Discriminant/src/0616026_HW2.py
+++ hw2-Discriminant/src/0616026_HW2.py
@@ -46,16 +46,11 @@ def cal_within_class_scatter_matrix(classified_vector, m0, m1):
         for data in classified_vector[label]:
             diff = np.array([data - m])
             dot = np.matmul(diff.T, diff)
-            print('data-m', diff)
-            print('dot', dot)
-            print()
 
             if label not in classified_sum:
                 classified_sum[label] = dot
             else:
                 classified_sum[label] += dot
-
-    print('classified_sum', classified_sum)
 
     sw = classified_sum[0] + classified_sum[1]
     print(f'Within-class scatter matrix SW:\n{sw}')
@@ -85,13 +80,13 @@ def project_data(datasets, w):
     for data in datasets:
         x_proj = (a * data[0] + data[1]) * c
         y_proj = b * x_proj
-        projtion_vector = [x_proj[0], y_proj[0]]
+        projection_vector = [x_proj[0], y_proj[0]]
         if not projection_array.any():
-            projection_array = np.array([projtion_vector])
+            projection_array = np.array([projection_vector])
         else:
             projection_array = np.append(
                 projection_array,
-                [projtion_vector],
+                [projection_vector],
                 axis=0
             )
     return projection_array
@@ -111,34 +106,9 @@ def FLD(data, sw, sb, m0, m1, w):
     return a/b
 
 
-def main():
-    # Read the datasets
-    x_train = np.load(x_train_data_path)
-    y_train = np.load(y_train_data_path)
-    x_test = np.load(x_test_data_path)
-    y_test = np.load(y_test_data_path)
-
-    data_x = x_train
-    data_y = y_train
-    # data_x = np.array([[1.0, 1.0], [2.0, 2.3], [-6.5, 1.6], [1.0, -1.0], [2.0, -2.5]])
-    # data_y = np.array([1, 1, 1, 0, 0])
-    train_classified_vector = classify_data(x_train, y_train)
-    m0, m1 = cal_mean(train_classified_vector)
-    sw = cal_within_class_scatter_matrix(train_classified_vector, m0, m1)
-    sb = cal_between_class_scatter_matrix(m0, m1)
-    w = cal_weight(sw, m0, m1)
-    
-    # print(x_train)
-    # print(y_train)
-    # print(x_test)
-    # print(y_test)
-
-
-    test_classified_vector = classify_data(x_test, y_test)
-    # test_classified_vector = classify_data(x_train, y_train)
-    
-    for label in test_classified_vector:
-        datasets = test_classified_vector[label]
+def plot_results(classified_vector, w):
+    for label in classified_vector:
+        datasets = classified_vector[label]
         datasets_project = project_data(datasets, w)
         dot_style = 'ro' if label == 0 else 'bo'
 
@@ -149,7 +119,6 @@ def main():
                 color='steelblue',
                 linewidth=0.5
             )
-
         plt.plot(
             [data[0] for data in datasets],
             [data[1] for data in datasets],
@@ -163,11 +132,30 @@ def main():
 
     slope = (w[1] / w[0])[0]
     x = np.linspace(-4, 4, 1000)
-    plt.plot(x, slope * x )
+    plt.plot(x, slope * x)
     plt.title(f'Projection Line: w={slope}, b={0.0}')
     plt.show()
 
-    
+
+def main():
+    # Read the datasets
+    x_train = np.load(x_train_data_path)
+    y_train = np.load(y_train_data_path)
+    x_test = np.load(x_test_data_path)
+    y_test = np.load(y_test_data_path)
+
+    # data_x = np.array([[1.0, 1.0], [2.0, 2.3], [-6.5, 1.6], [1.0, -1.0], [2.0, -2.5]])
+    # data_y = np.array([1, 1, 1, 0, 0])
+    train_classified_vector = classify_data(x_train, y_train)
+    m0, m1 = cal_mean(train_classified_vector)
+    sw = cal_within_class_scatter_matrix(train_classified_vector, m0, m1)
+    sb = cal_between_class_scatter_matrix(m0, m1)
+    w = cal_weight(sw, m0, m1)
+
+
+    test_classified_vector = classify_data(x_test, y_test)
+    # test_classified_vector = classify_data(x_train, y_train)
+    plot_results(test_classified_vector, w)
 
 
 if __name__ == '__main__':
